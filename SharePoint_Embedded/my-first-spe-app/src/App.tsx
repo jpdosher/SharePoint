540b9130e6c2e3e4ab484798376a6b723bc76ea0
Jean-Paul Dosher <87234390+jpdosher@users.noreply.github.com>
2024-03-17 17:57:39 -0300

    Embedded
    First app test with SP Embedded

---
+++ SharePoint_Embedded/my-first-spe-app/src/App.tsx
@@ -0,0 +1,88 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Providers, ProviderState } from "@microsoft/mgt-element";
+import { Login } from "@microsoft/mgt-react";
+import {
+  FluentProvider,
+  Text,
+  webLightTheme,
+} from "@fluentui/react-components";
+import {
+  InteractionRequiredAuthError,
+  PublicClientApplication,
+} from "@azure/msal-browser";
+import "./App.css";
+import * as Scopes from "./common/scopes";
+import * as Constants from "./common/constants";
+
+function useIsSignedIn() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    const updateState = async () => {
+      const provider = Providers.globalProvider;
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+    };
+
+    Providers.onProviderUpdated(updateState);
+    updateState();
+
+    return () => {
+      Providers.removeProviderUpdatedListener(updateState);
+    };
+  }, []);
+
+  return isSignedIn;
+}
+
+export default function App() {
+  const isSignedIn = useIsSignedIn();
+
+  const promptForContainerConsent = async (
+    event: CustomEvent<undefined>
+  ): Promise<void> => {
+    const containerScopes = {
+      scopes: [Scopes.SPEMBEDDED_FILESTORAGECONTAINER_SELECTED],
+      redirectUri: `${window.location.protocol}://${window.location.hostname}${
+        window.location.port === "80" || window.location.port === "443"
+          ? ""
+          : ":" + window.location.port
+      }`,
+    };
+
+    const msalInstance = new PublicClientApplication({
+      auth: {
+        clientId: Constants.CLIENT_ENTRA_APP_CLIENT_ID,
+        authority: Constants.CLIENT_ENTRA_APP_AUTHORITY,
+      },
+      cache: {
+        cacheLocation: "localStorage",
+        storeAuthStateInCookie: false,
+      },
+    });
+
+    msalInstance
+      .acquireTokenSilent(containerScopes)
+      .then((response) => {
+        console.log("tokenResponse", JSON.stringify(response));
+      })
+      .catch(async (error) => {
+        if (error instanceof InteractionRequiredAuthError) {
+          return msalInstance.acquireTokenPopup(containerScopes);
+        }
+      });
+  };
+
+  return (
+    <FluentProvider theme={webLightTheme}>
+      <div className="App">
+        <Text size={900} weight="bold">
+          Sample SPA SharePoint Embedded App
+        </Text>
+        <Login loginCompleted={promptForContainerConsent} />
+        <div></div>
+      </div>
+    </FluentProvider>
+  );
+}
---
+++ SharePoint_Embedded/my-first-spe-app/src/common/constants.ts
@@ -0,0 +1,4 @@
+export const CLIENT_ENTRA_APP_CLIENT_ID = '7418d23e-480a-4bc8-a04c-70065e2f7653';
+export const CLIENT_ENTRA_APP_AUTHORITY = 'https://login.microsoftonline.com/5be31d7e-cdfa-4200-8a1d-7e8a00e11d2e/';
+export const API_SERVER_URL = 'http://localhost:3001';
+export const CONTAINER_TYPE_ID = '648fa049-58fa-0c42-0e1b-b0b527cf4ef7';
